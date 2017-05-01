48e6f8e0154e8d951940e9c40d0e70d9d5230b00
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-01 12:22:59 -0700

    added SlackChannel code from morning ruby code into newly renamed file

---
+++ lib/slack_channel.rb
@@ -0,0 +1,50 @@
+class SlackChannel
+
+  class SlackChannel < StandardError
+  end
+
+  # WE DO NOT WANT TO ADD SLACK TOKEN TREAT LIKE OAUTH ID
+  BASE_URL = "https://slack.com/api/"
+
+  attr_reader :name
+
+  def initialize(data)
+    @name = name
+  end
+
+  def send(message)
+
+    query_params = {
+      "token" => TOKEN,
+      "channel" => @name,
+      "text" => message,
+      "username" => "Beyonce",
+      "icon_emoji" => ":information_desk_person:",
+      "as_user" => false
+    }
+
+    url = "#{BASE_URL}chat.postMessage"
+    response = HTTParty.post(url, query: query_params)
+
+    if response["ok"]
+      puts "Everything went swell"
+    else
+      raise SlackException.new(response["error"])
+    end
+  end
+
+  def self.all
+    url = "#{BASE_URL}channels.list?token=#{TOKEN}"
+    response = HTTParty.get(url).parsed_response
+
+    if response["ok"]
+      channel_list = response["channels"].map do |channel_data|
+        self.new(channel_data)
+      end
+
+      return channel_list
+    else
+      puts "Oh no there was an error: #{response["error"]}"
+    end
+  end
+end
